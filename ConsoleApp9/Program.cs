89cdab522c0170363265440b0cb57dd1748baa3e
exuteboll <exuteboll4@gmail.com>
2025-05-29 11:17:36 +0300

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,27 @@
+﻿using RectangleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите длину первой стороны: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите длину второй стороны: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+
+            Console.WriteLine($"Площадь прямоугольника: {rectangle.Area}");
+            Console.WriteLine($"Площадь прямоугольника: {rectangle.Perimeter}");
+            Console.ReadKey();
+        }
+    }
+}
